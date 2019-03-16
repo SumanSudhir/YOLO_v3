40e93a627d892e267f4dda5f53eef93727f8891e
SudhirSuman <alokiitb11@gmail.com>
2019-03-16 19:52:39 +0530

    Network completed

--- darknet.py
+++ darknet.py
@@ -179,3 +179,59 @@ class Darknet(nn.Module):
         super(Darknet, self).__init__()
         self.blocks = parse_cfg(cfgfile)
         self.net_info, self.module_list = create_modules(self.blocks)
+
+#Implementation of the forward pass of the network
+def forward(self, x, CUDA):
+    modules = self.blocks[1:]
+    outputs = {}                         #We cache the output of the route layer
+
+    write = 0
+    for i, module = in enumerate(modules):
+        module_type = (module["type"])
+
+        #for conv and upsample layers
+        if module_type == "convolutional" or module_type == "upsample":
+            x = self.module_list[i](x)
+
+
+        #Route Layer or shortcut layer
+        elif module_type == "route":
+            layers = module["layers"]
+            layers = [int(a) for a in layers]
+
+            if(layers[0]) > 0:
+                layers[0] = layers[0] - i
+
+            if len(layers) == 1:
+                x = outputs[i + (layers[0])]
+
+            else:
+                if (layers[1]) > 0:
+                    layers[1] = layers[1] - i
+
+                map1 = outputs[i + layers[0]]
+                map2 = outputs[i + layers[1]]
+
+                x = torch.cat((map1, map2), 1)
+
+        elif module_type == "shortcut":
+            from_ = int(module["from"])
+            x = outputs[i-1] + outputs[i + from_]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0
